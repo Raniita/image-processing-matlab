44b8dfd1077937373d9a9936f0ed116b23017be8
Raniita <efdezsanchez@gmail.com>
2021-11-16 22:54:36 +0100

    feat: clean code pseudocoloracion

--- 8-Pseudocoloracion/pseudocoloracion.m
+++ 8-Pseudocoloracion/pseudocoloracion.m
@@ -1,37 +1,17 @@
-% Duda: Se pierde relacion de aspecto????
-
 img_rgb = 'pseudocoloracion.jpg';
- 
 img = imread(img_rgb);
 %img = mat2gray(img,[0 255]);
 img = rgb2gray(img);
 
-figure
-imshow(img)
-title('grey')
-
-X = grayslice(img, 16);
+img_colored = grayslice(img, 64);
 
 figure
-%subplot(1,2,1),
+subplot(1,2,1),
 subimage(img);
 axis off image,
-colormap gray,
-title('Imagen Original')
+title('Imagen original en escala de gris')
 
-figure
-%subplot(1,2,2),
-subimage(X, copper(16));
+subplot(1,2,2),
+subimage(img_colored, copper(64));
 axis off image,
-title('Más azulado, algo más de detalle')
-
-%subplot(1,4,3),
-%subimage(X, jet(16));
-%axis off square,
-%title('Jet, más detalle')
-
-%subplot(1,4,4),
-%subimage(X, copper(16));
-%axis off square,
-%title('Copper colormap, más luminoso que otros')
-
+title('Pseudocoloración con cobre')
