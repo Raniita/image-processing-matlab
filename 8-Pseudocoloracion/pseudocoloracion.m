d2cc8e82371970c91985dc2a7733f71a8abf424b
Raniita <efdezsanchez@gmail.com>
2021-11-16 11:01:10 +0100

    fix: trying to fix aspect ratio

--- 8-Pseudocoloracion/pseudocoloracion.m
+++ 8-Pseudocoloracion/pseudocoloracion.m
@@ -1,31 +1,37 @@
+% Duda: Se pierde relacion de aspecto????
+
 img_rgb = 'pseudocoloracion.jpg';
  
 img = imread(img_rgb);
 %img = mat2gray(img,[0 255]);
 img = rgb2gray(img);
- 
+
+figure
+imshow(img)
+title('grey')
+
 X = grayslice(img, 16);
 
-figure,set(gcf, 'Name', 'Contrast Adjustment', 'Position', get(0,'Screensize'))
-
-subplot(2,2,1),
+figure
+%subplot(1,2,1),
 subimage(img);
 axis off square,
 colormap gray,
 title('Imagen Original')
 
-subplot(2,2,2),
-subimage(X, bone(16));
+figure
+%subplot(1,2,2),
+subimage(X, jet(16));
 axis off square,
 title('Más azulado, algo más de detalle')
 
-subplot(2,2,3),
-subimage(X, jet(16));
-axis off square,
-title('Jet, más detalle')
+%subplot(1,4,3),
+%subimage(X, jet(16));
+%axis off square,
+%title('Jet, más detalle')
 
-subplot(2,2,4),
-subimage(X, copper(16));
-axis off square,
-title('Copper colormap, más luminoso que otros')
+%subplot(1,4,4),
+%subimage(X, copper(16));
+%axis off square,
+%title('Copper colormap, más luminoso que otros')
 
