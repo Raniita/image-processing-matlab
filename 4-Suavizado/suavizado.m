6a65f3e828e66aefce10606f0aec6436108b4dfd
Raniita <efdezsanchez@gmail.com>
2021-11-16 02:54:46 +0100

    feat: suavizado

---
+++ 4-Suavizado/suavizado.m
@@ -0,0 +1,17 @@
+% 4 - Suavizado
+clear
+
+cte_gauss = 3;
+
+img_rgb = imread('suavizado.jpg');
+img_filt = imgaussfilt(img_rgb, cte_gauss);
+
+% Representamos
+figure
+subplot(1,2,1)
+imshow(img_rgb)
+title('Imagen original')
+
+subplot(1,2,2)
+imshow(img_filt)
+title('Imagen filtrada')
